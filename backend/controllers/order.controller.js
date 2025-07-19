2917f2e391b30e03c91eac7af9cd19cdc9f39a85
huynhnguyenday <nguyenhuuhuynh2702@gmail.com>
2025-07-19 14:31:25 +0700

    change domain

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -23,7 +23,7 @@ export const getOrder = async (req, res) => {
         ...item,
         product: {
           ...item.product,
-          image: `https://chuz1qpd.up.railway.app/assets/${item.product.image}`,
+          image: `https://bamosbe-production.up.railway.app/assets/${item.product.image}`,
         },
       })),
     }));
@@ -283,7 +283,7 @@ export const getOrderByToken = async (req, res) => {
         ...item,
         product: {
           ...item.product,
-          image: `https://chuz1qpd.up.railway.app/assets/${item.product.image}`,
+          image: `https://bamosbe-production.up.railway.app/assets/${item.product.image}`,
         },
       })),
     }));
